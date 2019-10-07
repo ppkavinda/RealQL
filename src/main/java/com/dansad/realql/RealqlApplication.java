e3925ea40b4b05528d787dfceca66592c375cd5c
prasad kavinda <pp.kavinda@gmail.com>
2019-10-07 17:54:02 +0530

    initial commit

---
+++ src/main/java/com/dansad/realql/RealqlApplication.java
@@ -0,0 +1,13 @@
+package com.dansad.realql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealqlApplication.class, args);
+	}
+
+}
